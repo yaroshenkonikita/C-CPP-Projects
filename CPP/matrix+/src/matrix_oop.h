ac5fa8a6070a34895a2241c8bc780414e03f358f
tokimikichika <yaroshenkonikita4@gmail.com>
2023-05-04 21:48:52 +0300

    try rework

--- CPP/matrix+/src/matrix_oop.h
+++ CPP/matrix+/src/matrix_oop.h
@@ -1,96 +1,214 @@
 #include <algorithm>
 #include <cmath>
+#include <cstddef>
+#include <cstdint>
 #include <stdexcept>
+#include <memory>
+#include <limits>
 
+namespace s21 {
+
+template<class T>
 class Matrix {
  public:
-  // Default constructor
-  Matrix();
-  // Destructor
-  ~Matrix();
-  // Parameterized constructor
-  Matrix(int Rows, int Columns);
-  // Copy constructor
-  Matrix(const Matrix &other);
-  // Move constructor
-  Matrix(Matrix &&other);
+  Matrix() : rows_(3), columns_(3), matrix_(new T[9]{}) {}
+  ~Matrix() {}
+  Matrix(std::size_t rows, std::size_t columns) : rows_(rows), columns_(columns) {
+    if (!rows_ || !columns_) {
+      throw std::invalid_argument("Arguments cannot be zero");
+    } else if (Size() >= std::numeric_limits<T>::max()) {
+      throw std::invalid_argument("Over valueeric limits");
+    }
+    matrix_ = new T[Size()]{};
+  }
+  Matrix(const Matrix &other) : rows_(other.rows_), columns_(other.columns_) {
+    matrix_ = new T[Size()]{};
+    std::copy_n(other.matrix_, Size(), matrix_);
+  }
+  Matrix(Matrix &&other) {
+    rows_ = std::exchange(other.rows_, 3);
+    columns_ = std::exchange(other.columns_, 3);
+    matrix_ = std::exchange(other.matrix_, new T[9]{0});
+  }
 
-  // Accessors
-  int GetColumns() const noexcept;
-  int GetRows() const noexcept;
-  // Elements in Matrix
-  int Size() const noexcept;
+  std::size_t GetColumns() const noexcept { return columns_; }
+  std::size_t GetRows() const noexcept { return rows_; }
+  std::uint64_t Size() const noexcept { return columns_ * rows_; }
 
-  // Mutator
-  void Set(int size_rows, int size_columns);
-  void SetRows(int size_rows);
-  void SetColumns(int size_columns);
+  void Set(std::size_t rows, std::size_t columns) {
+    Matrix temp(rows, columns);
+    for (std::size_t row = 0; row < std::min(rows_, rows); ++row) {
+      for (std::size_t column = 0; column < std::min(columns_, columns); ++column) {
+        temp(row, column) = (*this)(row, column);
+      }
+    }
+    *this = std::move(temp);
+  }
+  void SetRows(const std::size_t rows) { Set(rows, columns_); }
+  void SetColumns(const std::size_t columns) { Set(rows_, columns); }
   
-  // Matrix indexing
-  // Set
-  double &operator()(const int rows, const int columns);
-  // Get
-  const double &operator()(const int rows, const int columns) const;
+  T &operator()(const std::size_t rows, const std::size_t columns) {
+    if (rows >= rows_ || columns >= columns_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return matrix_[rows * columns_ + columns];
+  }
+  const T &operator()(const std::size_t rows, const std::size_t columns) const {
+    if (rows >= rows_ || columns >= columns_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return matrix_[rows * columns_ + columns];
+  }
 
-  // Operator equivalent to SumMatrix
-  Matrix &operator+=(const Matrix &other);
-  // Operator equivalent to MulMatrix
-  Matrix &operator*=(const Matrix &other);
-  // Operator equivalent to MulNumber
-  Matrix &operator*=(double num);
-  // Operator equivalent to SubMatrix
-  Matrix &operator-=(const Matrix &other);
+  Matrix &operator+=(const Matrix &other) { return SumMatrix(other), *this; }
+  Matrix &operator*=(const Matrix &other) { return MulMatrix(other), *this; }
+  Matrix &operator*=(const T &value) { return Mulvalueber(value), *this; }
+  Matrix &operator-=(const Matrix &other) { return SubMatrix(other), *this; }
 
-  // Operators equivalent to EqMatrix
-  bool operator==(const Matrix &other) const;
-  // Operators equivalent to !EqMatrix
-  bool operator!=(const Matrix &other) const;
+  bool operator==(const Matrix &other) const { return EqMatrix(other); }
+  bool operator!=(const Matrix &other) const { return !EqMatrix(other); }
 
-  // Addition of two matrices
-  [[nodiscard]] Matrix operator+(const Matrix &other) const;
-  // Subtraction of one matrix from another
-  [[nodiscard]] Matrix operator-(const Matrix &other) const;
-  // Matrix multiplication
-  [[nodiscard]] Matrix operator*(const Matrix &other) const;
-  // Мatrix multiplication by a number
-  friend Matrix operator*(double num, Matrix &other);
-  friend Matrix operator*(Matrix &other, double num);
+  [[nodiscard]] Matrix operator+(Matrix other) const { return other += *this; }
+  [[nodiscard]] Matrix operator-(Matrix other) const { return other -= *this; }
+  [[nodiscard]] Matrix operator*(Matrix other) const { return other *= *this; }
 
-  // Сopying of values from one matrix to another one
-  Matrix &operator=(const Matrix &other);
-  // Move values from one matrix to another
-  Matrix &operator=(Matrix &&other);
+  Matrix &operator=(const Matrix &other) {
+    if (this != &other) {
+      Matrix temp(other);
+      *this = std::move(temp);
+    }
+    return *this;
+  }
+  Matrix &operator=(Matrix &&other) {
+    if (this != &other) {
+      rows_ = std::exchange(other.rows_, 3);
+      columns_ = std::exchange(other.columns_, 3);
+      matrix_.reset(std::exchange(other.matrix_, new T[9]{0}));
+    }
+    return *this;
+  }
 
-  // Checks matrices for equality with each other
-  bool EqMatrix(const Matrix &other) const;
+  bool EqMatrix(const Matrix &other) const {
+    if (rows_ != other.rows_ || columns_ != other.columns_) {
+      return false;
+    }
+    for (std::uint64_t i = 0, max_index = Size(); i < max_index; ++i) {
+      if (matrix_[i] != other.matrix_[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  // Adds the second matrix to the current one
-  void SumMatrix(const Matrix &other);
-  // Subtracts another matrix from the current one
-  void SubMatrix(const Matrix &other);
-  // Multiplies the current matrix by a number
-  void MulNumber(double num);
-  // Multiplies the current matrix by the second matrix
-  void MulMatrix(const Matrix &other);
+  void SumMatrix(const Matrix &other) {
+    CheckEqSize(other);
+    for (std::uint64_t i = 0, max_index = Size(); i < max_index; ++i) {
+      matrix_[i] += other.matrix_[i];
+    }
+  }
+  void SubMatrix(const Matrix &other) {
+    CheckEqSize(other);
+    for (std::uint64_t i = 0, max_index = Size(); i < max_index; ++i) {
+      matrix_[i] -= other.matrix_[i];
+    }
+  }
+  void Mulvalueber(const T value) {
+    for (std::uint64_t i = 0, max_index = Size(); i < max_index; ++i) {
+      matrix_[i] *= value;
+    }
+  }
+  void MulMatrix(const Matrix &other) {
+    if (columns_ != other.rows_) {
+      throw std::invalid_argument(
+          "Wrong matrix, different Size first matrix "
+          "columns and second matrix rows");
+    }
+    Matrix result(rows_, other.columns_);
+    for (std::size_t i = 0; i < result.rows_; ++i) {
+      for (std::size_t j = 0; j < result.columns_; ++j) {
+        for (std::size_t k = 0; k < columns_; ++k) {
+          result(i, j) += (*this)(i, k) * other(k, j);
+        }
+      }
+    }
+    *this = std::move(result);
+  }
 
-  // Creates a new transposed matrix from the current one and returns it
-  [[nodiscard]] Matrix Transpose() const;
-  // Creates a new minor matrix from the current one and returns it
-  [[nodiscard]] Matrix MinorMatrix(int discard_row,
-                                      int discard_column) const;
-  // Calculates the algebraic addition matrix of the current one and returns it
-  [[nodiscard]] Matrix CalcComplements() const;
-  // Calculates and returns the determinant of the current matrix
-  double Determinant() const;
-  // Calculates and returns the inverse matrix
-  [[nodiscard]] Matrix InverseMatrix() const;
+  [[nodiscard]] Matrix Transpose() const {
+    Matrix result(columns_, rows_);
+    for (std::size_t i = 0; i < rows_; ++i) {
+      for (std::size_t j = 0; j < columns_; ++j) {
+        result(j, i) = (*this)(i, j);
+      }
+    }
+    return result;
+  }
+  [[nodiscard]] Matrix MinorMatrix(std::size_t discard_row, std::size_t discard_column) const {
+    if (rows_ == 1 || columns_ == 1 || discard_row >= rows_ || discard_column >= columns_) {
+      throw std::invalid_argument("Invalid argument, impossible minor matrix");
+    }
+    Matrix result(rows_ - 1, columns_ - 1);
+    for (int i = 0, index_row = 0; i < rows_; ++i) {
+      for (int j = 0, index_column = 0; j < columns_; ++j) {
+        if (i == discard_row || j == discard_column) continue;
+        result(index_row, index_column++) = (*this)(i, j);
+      }
+      if (i != discard_row) { index_row++; }
+    }
+    return result;
+  }
+  [[nodiscard]] Matrix CalcComplements() const {
+    if (rows_ != columns_ || rows_ < 2) {
+      throw std::invalid_argument(
+          "Impossible calc complements, matrix is not square");
+    }
+    Matrix result(rows_, columns_);
+    for (int i = 0; i < rows_; ++i) {
+      for (int j = 0; j < columns_; ++j) {
+        result(i, j) = MinorMatrix(i, j).Determinant() * pow(-1, i + j);
+      }
+    }
+    return result;
+  }
+  T Determinant() const {
+    if (rows_ != columns_) {
+      throw std::invalid_argument(
+          "Impossible determinant matrix, matrix is not square");
+    }
+    if (rows_ == 1) {
+      return matrix_[0];
+    } else if (rows_ == 2) {
+      return matrix_[0] * matrix_[3] - matrix_[1] * matrix_[2];
+    }
+    T determinant = 0.0;
+    for (int j = 0; j < rows_; ++j) {
+      determinant += matrix_[j] * MinorMatrix(0, j).Determinant() * pow(-1, j);
+    }
+    return determinant;
+  }
+  [[nodiscard]] Matrix InverseMatrix() const {
+    T det = Determinant();
+    if (fabs(det) < 1e-6) {
+      throw std::invalid_argument("Impossible inverse matrix, determinant is 0");
+    }
+    Matrix temp = CalcComplements().Transpose();
+    return temp *= 1 / det;
+  }
 
  private:
-  // Attributes
-  double *matrix_;
-  int rows_, columns_;
+  std::shared_ptr<T> matrix_;
+  std::size_t rows_, columns_;
   
-  // Check on equal size
-  void CheckEqSize(const Matrix &other) const;
-
+  void CheckEqSize(const Matrix &other) const {
+    if (rows_ != other.rows_ || columns_ != other.columns_) {
+      throw std::invalid_argument("Wrong matrix, different Size");
+    }
+  }
 };
+
+template<class T>
+Matrix<T> operator*(T value, Matrix<T> other) { return other += value; }
+template<class T>
+Matrix<T> operator*(Matrix<T> other, T value) { return other += value; }
+
+} // namespase s21
